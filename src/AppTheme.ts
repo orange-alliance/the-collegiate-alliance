1763d2d6fec2d753265c5a0435250173883ba529
Soren Zaiser <soren.zaiser@gmail.com>
2022-02-11 18:09:26 -0500

    Update Season

--- src/AppTheme.ts
+++ src/AppTheme.ts
@@ -2,7 +2,7 @@ import {createMuiTheme} from "@material-ui/core/styles";
 
 export const CURRENT_SEASON = "22";
 export const CURRENT_SEASON_NAME = "Rapid React";
-export const CURRENT_SEASON_EVENT_ID = "22-FCC-TEST";
+export const CURRENT_SEASON_EVENT_ID = "22-FCC-GR";
 
 export default createMuiTheme({
   palette: {
